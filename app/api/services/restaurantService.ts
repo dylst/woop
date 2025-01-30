ca378650dba7ffc6c584a6597037a0e3bfbf3131
JojayD <professional.josephd@gmail.com>
2025-01-30 15:55:15 -0800

    pushing my changes

--- app/api/services/restaurantService.ts
+++ app/api/services/restaurantService.ts
@@ -1,5 +1,5 @@
 import { axiosInstance } from "../config/axios";
-import { Restaurant } from "../../../types/restaurant.types";
+import { Restaurant } from "@/types/restaurant.types";
 
 export const restaurantService = {
 	getRestaurantsByState: async (
@@ -16,21 +16,55 @@ export const restaurantService = {
 				`/restaurants/location/state/${state}/${page}`
 			);
 
-			console.log("Response Data:", response.data);
+			console.log("Raw API Response:", response.data);
 
-			if (!response.data) {
-				throw new Error("No data received from API");
-			}
+			// if (!response.data || !Array.isArray(response.data.restaurants)) {
+			// 	throw new Error("API did not return an expected 'restaurants' array.");
+			// }
 
-			return response.data;
+			return response.data.restaurants; 
 		} catch (error) {
 			console.error(
 				"Error fetching restaurants:",
-				error instanceof Error ? error.message : 'An unknown error occurred'
+				error instanceof Error ? error.message : "An unknown error occurred"
+			);
+			throw error;
+		}
+	},
+	getRestaurantByZipCode: async (
+		zipCode: string,
+		page: number = 0
+	): Promise<Restaurant[]> => {
+		try {
+			console.log(
+				"Fetching restaurants from:",
+				`${axiosInstance.defaults.baseURL}/restaurants/location/${zipCode}/${page}`
+			);
+
+			const response = await axiosInstance.get(
+				`restaurants/location/zipcode/${zipCode}/${page}`
+			);
+
+			console.log("Raw API Response:", response.data);
+
+			if (!response.data || !Array.isArray(response.data.restaurants)) {
+				console.error("API did not return an expected 'restaurants' array.");
+
+			}
+
+			return response.data.restaurants;
+		} catch (error) {
+			console.error(
+				"Error fetching restaurants:",
+				error instanceof Error ? error.message : "An unknown error occurred"
 			);
 			throw error;
 		}
 	},
 };
+
+
+
+
 
 export default restaurantService;
