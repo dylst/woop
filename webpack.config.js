91948681fc0ddcb56d751ca518a3792c00424312
JojayD <professional.josephd@gmail.com>
2025-02-10 15:51:28 -0800

    add restaurant location search by city and enhance restaurant data structure

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+
+module.exports = async function (env, argv) {
+    const config = await createExpoWebpackConfigAsync(env, argv);
+
+    config.resolve.alias['react-native-maps'] = '@teovilla/react-native-web-maps';
+
+    return config;
+};
